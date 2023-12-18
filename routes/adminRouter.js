604fa609a4e087914980bce435e920fa3e802ddf
Vladyslav <vyakovenko@dirks-group.de>
2023-12-18 14:55:01 +0100

    add version to the footer

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
 
 const isLogIn = require('../controllers/auth/isLogIn');
 const isAdmin = require('../controllers/auth/isAdmin');
@@ -10,16 +11,17 @@ const adminDelete = require('../controllers/admin/adminDelete');
 const adminConfirm = require('../controllers/admin/adminConfirm');
 
 // GET
+const title = "WRO Dirks App | Admin panel";
+const pageName = "Admin panel";
+const version = process.env.APP_VERSION;
 
 router.get('/admin', isLogIn, isAdmin, adminSelect, (req, res) => {
     if (req.user && req.admin) {
-        const title = "WRO Dirks App | Admin panel";
-        const pageName = "Admin panel";
         const pageTitle = "Panel zarządzania kontami i uprawnieniami";
         const admin = req.admin;
         const howMany = req.howMany;
         const unconfirmedSumNo = req.unconfirmedSumNo;
-        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo})
+        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, version})
     } else {
         res.redirect('/start')
     }
@@ -27,14 +29,12 @@ router.get('/admin', isLogIn, isAdmin, adminSelect, (req, res) => {
 
 router.get('/admin/unconfirmedUsers', isLogIn, isAdmin, adminSelect, (req, res) => {
     if (req.user && req.admin) {
-        const title = "WRO Dirks App | Admin panel";
-        const pageName = "Admin panel";
         const pageTitle = `Niepotwierdzone konta (${req.unconfirmedUsers.length})`;
         const admin = req.admin;
         const howMany = req.howMany;
         const unconfirmedSumNo = req.unconfirmedSumNo;
         const unconfirmedUsers = req.unconfirmedUsers;
-        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, unconfirmedUsers})
+        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, unconfirmedUsers, version})
     } else {
         res.redirect('/start')
     }
@@ -42,14 +42,12 @@ router.get('/admin/unconfirmedUsers', isLogIn, isAdmin, adminSelect, (req, res) 
 
 router.get('/admin/confirmedUsers', isLogIn, isAdmin, adminSelect, (req, res) => {
     if (req.user && req.admin) {
-        const title = "WRO Dirks App | Admin panel";
-        const pageName = "Admin panel";
         const pageTitle = `Potwierdzone konta (${req.confirmedUsers.length})`;
         const admin = req.admin;
         const howMany = req.howMany;
         const unconfirmedSumNo = req.unconfirmedSumNo;
         const confirmedUsers = req.confirmedUsers;
-        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, confirmedUsers})
+        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, confirmedUsers, version})
     } else {
         res.redirect('/start')
     }
@@ -57,14 +55,12 @@ router.get('/admin/confirmedUsers', isLogIn, isAdmin, adminSelect, (req, res) =>
 
 router.get('/admin/unconfirmedAccesses', isLogIn, isAdmin, adminSelect, (req, res) => {
     if (req.user && req.admin) {
-        const title = "WRO Dirks App | Admin panel";
-        const pageName = "Admin panel";
         const pageTitle = `Niepotwierdzone działy (${req.unconfirmedAccesses.length})`;
         const admin = req.admin;
         const howMany = req.howMany;
         const unconfirmedSumNo = req.unconfirmedSumNo;
         const unconfirmedAccesses = req.unconfirmedAccesses;
-        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, unconfirmedAccesses})
+        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, unconfirmedAccesses, version})
     } else {
         res.redirect('/start')
     }
@@ -72,14 +68,12 @@ router.get('/admin/unconfirmedAccesses', isLogIn, isAdmin, adminSelect, (req, re
 
 router.get('/admin/confirmedAccesses', isLogIn, isAdmin, adminSelect, (req, res) => {
     if (req.user && req.admin) {
-        const title = "WRO Dirks App | Admin panel";
-        const pageName = "Admin panel";
         const pageTitle = `Potwierdzone działy (${req.confirmedAccesses.length})`;
         const admin = req.admin;
         const howMany = req.howMany;
         const unconfirmedSumNo = req.unconfirmedSumNo;
         const confirmedAccesses = req.confirmedAccesses;
-        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, confirmedAccesses})
+        res.render('admin', {title, pageName, pageTitle, admin, howMany, unconfirmedSumNo, confirmedAccesses, version})
     } else {
         res.redirect('/start')
     }
--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
 
 const isLogIn = require('../controllers/auth/isLogIn');
 const isAdmin = require('../controllers/auth/isAdmin');
@@ -10,18 +11,19 @@ const profileUpdate = require('../controllers/profile/profileUpdate');
 const departmentAccess = require('../controllers/profile/departmentAccess');
 
 // GET
+const title = "WRO Dirks App | Profile";
+const pageName = "Profile"
+const version = process.env.APP_VERSION;
 
 router.get('/profile', isLogIn, isAdmin, adminSelect, profileSelect, (req, res) => {
     if (req.user) {
-        const title = "WRO Dirks App | Profile";
-        const pageName = "Profile";
         const admin = req.admin;
         const unconfirmedSumNo = req.unconfirmedSumNo;
         const { user, first_name, last_name } = req.userInfo[0];
         const departmentConfirmed = req.departmentConfirmed;
         const departmentUnconfirmed = req.departmentUnconfirmed;
         const departmentWithoutAccess = req.departmentWithoutAccess;
-        res.render('profile', {title, pageName, admin, unconfirmedSumNo, user, first_name, last_name, departmentConfirmed, departmentUnconfirmed, departmentWithoutAccess})
+        res.render('profile', {title, pageName, admin, unconfirmedSumNo, user, first_name, last_name, departmentConfirmed, departmentUnconfirmed, departmentWithoutAccess, version})
     } else {
         res.redirect('/start')
     }
